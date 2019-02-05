96ee37499c963b763d4e7bc632b1c3b8e55995a1
InvincibleZeal <ritesh.ganjewala17@gmail.com>
2019-02-05 19:36:17 +0530

    SVG spinner added to the input

--- projects/ngx-input-loader/src/lib/ngx-input-loader.directive.ts
+++ projects/ngx-input-loader/src/lib/ngx-input-loader.directive.ts
@@ -1,5 +1,4 @@
 import { Directive, OnChanges, AfterViewInit, SimpleChanges, ElementRef, Renderer2, Input } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
 
 @Directive({
     selector: '[ngxInputLoader]'
@@ -8,36 +7,46 @@ export class NgxInputLoaderDirective implements OnChanges, AfterViewInit {
     @Input() ngxInputLoader: Boolean;
     private inserted: Boolean = false;
 
-    constructor(private el: ElementRef, private renderer: Renderer2, private spinner: NgxSpinnerService) {
+    constructor(private el: ElementRef, private renderer: Renderer2) {
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        console.log('changes', changes);
+        console.log('OnChanges');
         this.setLoader();
     }
 
     ngAfterViewInit() {
-        console.log('dfdfchanges');
+        console.log('AfterViewInit');
         this.setLoader();
     }
 
     setLoader() {
-        if (!this.inserted) {
-            console.log('ngxInputLoader', this.ngxInputLoader);
-            console.log(this.el);
-            console.log(this.el.nativeElement);
-            let parent = this.el.nativeElement.parentElement
-            console.log(parent);
-            let spinner = this.renderer.createElement('ngx-spinner');
-            this.setAttributes(spinner)
-            this.renderer.insertBefore(parent, spinner, this.el.nativeElement);
-            this.inserted = true;
-            if (this.ngxInputLoader) this.spinner.show()
-            else this.spinner.hide();
-        }
-    }
+        let nextSibling = this.el.nativeElement.nextSibling;
+        let parent = this.el.nativeElement.parentElement;
 
-    setAttributes(spinner) {
-        this.renderer.setAttribute(spinner, 'bdColor', '');
+        if (!this.inserted && this.ngxInputLoader) {
+            let spinner = this.renderer.createElement('img');
+            let height = this.el.nativeElement.offsetHeight;
+            console.log(height);
+
+            this.renderer.setAttribute(spinner, 'src', 'data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background: none;"><circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#1d0e0b" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(281.902 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>')
+            this.renderer.setStyle(spinner, 'position', 'relative');
+            this.renderer.setStyle(spinner, 'left', `-${height}px`);
+            this.renderer.setStyle(spinner, 'top', `${height * 0.2}px`)
+            this.renderer.setStyle(spinner, 'height', `${height * 0.8}px`);
+            this.renderer.addClass(spinner, 'ngx-input-loader');
+            // adding after the element in DOM
+            if (nextSibling) {
+                this.renderer.insertBefore(parent, spinner, nextSibling);
+            } else {
+                this.renderer.appendChild(parent, spinner);
+            }
+            this.inserted = true;
+            console.log('CLICK ON THE INPUT TO REMOVE SPINNER');
+        } else if (this.inserted && !this.ngxInputLoader) {
+            this.renderer.removeChild(parent, parent.querySelector('.ngx-input-loader'));
+            this.inserted = false;
+            console.log('CLICK ON THE INPUT TO ADD SPINNER');
+        }
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
     title = 'InputLoaderLibrary';
+    loader = true;
 
 }
